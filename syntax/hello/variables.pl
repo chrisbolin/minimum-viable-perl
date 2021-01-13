1702513f1888eb66722757f875098d1c7061ffc7
Chris Bolin <bolin.chris@gmail.com>
2021-01-12 22:12:39 -0700

    init. first 2 parts

---
+++ syntax/hello/variables.pl
@@ -0,0 +1,12 @@
+my $nickname = 'CB';
+
+my @favorites = (1, 7, 42);
+
+my %profile = {name => "Chris Bolin", age => 36};
+
+print $nickname;
+print "\n";
+print $favorites[1];
+print "\n";
+print %profile{name};
+print "\n";
